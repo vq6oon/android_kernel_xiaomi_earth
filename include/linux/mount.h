386731f13a7f9017d53bdc5d2c15a9124d6bfae2
AttentionSeekr <90097027+rsuntk@users.noreply.github.com>
2025-08-31 09:17:47 +0700

    BACKPORT: KernelSU: Implement SuspiciousFS (SusFS) v1.5.9@8acbd107
    * Backported by @rsuntk
    * Removed SusFS feature: Sus_su
    
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>
    Co-authored-by: simonpunk <simonpunk2016@gmail.com>

--- include/linux/mount.h
+++ include/linux/mount.h
@@ -70,11 +70,18 @@ struct vfsmount {
 	struct dentry *mnt_root;	/* root of the mounted tree */
 	struct super_block *mnt_sb;	/* pointer to superblock */
 	int mnt_flags;
-	void *data;
 	ANDROID_KABI_RESERVE(1);
 	ANDROID_KABI_RESERVE(2);
 	ANDROID_KABI_RESERVE(3);
+#ifdef CONFIG_KSU_SUSFS
+	ANDROID_KABI_USE(4, u64 susfs_mnt_id_backup);
+#else
 	ANDROID_KABI_RESERVE(4);
+#endif
+#if defined(CONFIG_KSU_SUSFS) && !defined(ANDROID_KABI_RESERVE)
+	u64 susfs_mnt_id_backup;
+#endif
+	void *data;
 } __randomize_layout;
 
 struct file; /* forward dec */
